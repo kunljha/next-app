5bdce416d7dc9986b46ac0777ce12a2333c9baf2
kunljha <jha711kunal@gmail.com>
2021-11-20 22:53:59 +0530

    nested routing to get a article details

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,31 @@
+// import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const article = ({ article }) => {
+	// const router = useRouter()
+	// const { id } = router.query
+
+	return (
+		<>
+			<h1>{article.title}</h1>
+			<p>{article.body}</p>
+			<br />
+			<Link href='/'>Go Back &rarr;</Link>
+		</>
+	)
+}
+
+export const getServerSideProps = async (context) => {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+	)
+	const article = await res.json()
+
+	return {
+		props: {
+			article: article,
+		},
+	}
+}
+
+export default article
