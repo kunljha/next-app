fa23eeedd5aaa4cffcb48731be7ed26fe56e8f42
kunljha <jha711kunal@gmail.com>
2021-11-20 22:02:39 +0530

    using getStaticProps, defined Articles & ArticleItem components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
+import Articles from '../components/Articles'
 
-const Home = () => {
+const Home = ({ articles }) => {
 	return (
 		<div>
 			<Head>
@@ -15,8 +16,20 @@ const Home = () => {
 				<title>Dev Update</title>
 			</Head>
 			<h1>Hello from Next.js App</h1>
+			<Articles articles={articles} />
 		</div>
 	)
+}
+
+export const getStaticProps = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+	const articles = await res.json()
+
+	return {
+		props: {
+			articles,
+		},
+	}
 }
 
 export default Home
